494633717b8371dc951608875a16040afbfb498d
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-10-24 17:23:34 +0530

    Refactor database class to use TinyDBAsync

--- plugins/database.py
+++ plugins/database.py
@@ -1,13 +1,15 @@
-import motor.motor_asyncio
-from config import DB_NAME, DB_URI
+from tinydb_async import TinyDBAsync
+from tinydb import Query, where
+from config import DB_FILE
 
 class Database:
     
-    def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.users_col = self.db.users
-        self.channels_col = self.db.channels
+    def __init__(self, db_file):
+        self.db = TinyDBAsync(db_file)
+        self.users_col = self.db.table('users')
+        self.channels_col = self.db.table('channels')
+        self.User = Query()
+        self.Channel = Query()
 
     def new_user(self, id, name):
         return dict(
@@ -28,53 +30,54 @@ class Database:
     
     async def add_user(self, id, name):
         user = self.new_user(id, name)
-        await self.users_col.insert_one(user)
+        await self.users_col.insert(user)
     
     async def is_user_exist(self, id):
-        user = await self.users_col.find_one({'id':int(id)})
+        user = await self.users_col.get(self.User.id == int(id))
         return bool(user)
     
     async def total_users_count(self):
-        count = await self.users_col.count_documents({})
+        count = await self.users_col.count()
         return count
 
     async def get_all_users(self):
-        return self.users_col.find({})
+        # TinyDB returns a list of dicts, not a cursor.
+        return await self.users_col.all()
 
     async def delete_user(self, user_id):
-        await self.users_col.delete_many({'id': int(user_id)})
+        await self.users_col.remove(self.User.id == int(user_id))
 
     async def set_session(self, id, session):
-        await self.users_col.update_one({'id': int(id)}, {'$set': {'session': session}})
+        await self.users_col.update({'session': session}, self.User.id == int(id))
 
     async def get_session(self, id):
-        user = await self.users_col.find_one({'id': int(id)})
-        return user['session']
+        user = await self.users_col.get(self.User.id == int(id))
+        return user.get('session') if user else None
 
     async def add_channel(self, user_id, channel_id, base_username, interval):
         channel = self.new_channel(user_id, channel_id, base_username, interval)
-        await self.channels_col.insert_one(channel)
+        await self.channels_col.insert(channel)
 
     async def get_user_channels(self, user_id):
-        return await self.channels_col.find({'user_id': int(user_id), 'is_active': True}).to_list(None)
+        return await self.channels_col.search((self.Channel.user_id == int(user_id)) & (self.Channel.is_active == True))
 
     async def get_all_active_channels(self):
-        return await self.channels_col.find({'is_active': True}).to_list(None)
+        return await self.channels_col.search(self.Channel.is_active == True)
 
     async def stop_channel(self, channel_id):
-        await self.channels_col.update_one({'channel_id': int(channel_id)}, {'$set': {'is_active': False}})
+        await self.channels_col.update({'is_active': False}, self.Channel.channel_id == int(channel_id))
 
     async def resume_channel(self, channel_id):
-        await self.channels_col.update_one({'channel_id': int(channel_id)}, {'$set': {'is_active': True}})
+        await self.channels_col.update({'is_active': True}, self.Channel.channel_id == int(channel_id))
 
     async def delete_channel(self, channel_id):
-        await self.channels_col.delete_one({'channel_id': int(channel_id)})
+        await self.channels_col.remove(self.Channel.channel_id == int(channel_id))
 
     async def update_last_changed(self, channel_id, timestamp):
-        await self.channels_col.update_one({'channel_id': int(channel_id)}, {'$set': {'last_changed': timestamp}})
+        await self.channels_col.update({'last_changed': timestamp}, self.Channel.channel_id == int(channel_id))
 
     async def get_channel(self, channel_id):
-        return await self.channels_col.find_one({'channel_id': int(channel_id)})
+        return await self.channels_col.get(self.Channel.channel_id == int(channel_id))
 
-db = Database(DB_URI, DB_NAME)
-  
+db = Database(DB_FILE)
+
