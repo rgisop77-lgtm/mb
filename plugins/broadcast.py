2bdb4f98ef7f5b5dae3208b48b48e3f6b1852f10
onlyeRG <hindinarutoshippuden@gmail.com>
2025-10-24 17:38:17 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -72,4 +72,4 @@ async def verupikkals(bot, message):
     
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-  
+
